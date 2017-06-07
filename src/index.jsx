6c18413149f8cdd45c7d68b7de8edf404568e0bb
Jered Danielson <jered.danielson@hbo.com>
2017-06-07 16:57:45 -0700

    Fixed .babelrc missing, added reset.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 require("file?name=../public/favicon.ico!./favicon.ico"); // copy favicon.ico
-require("./index.scss");
+require("../node_modules/reset-css/reset.css"); // Reset CSS styles
+require("./index.scss"); // Main SASS/CSS
 
 import App from "./components/App";
 
